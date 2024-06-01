2c55068c47dfb826f492b9fec1a44917d55cb2f6
Nishat Nujhat <101088109+Nuzhat-here@users.noreply.github.com>
2024-06-01 14:00:07 +0600

    app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ with st.sidebar:
                             'Heart Disease Prediction',
                             'Parkinsons Prediction'],
                            menu_icon='laptop',
-                           icons=['capsule', 'heart', 'person walking'],
+                           icons=['capsule', 'heart', 'person-walking'],
                            default_index=0)
 
 
